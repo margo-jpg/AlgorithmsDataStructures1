32a0505fcd5a03245a1333635c9604c8fa7d664f
Margarita <Margarita@LAPTOP-42ALDNC5>
2025-04-13 19:17:10 +0300

    test

---
+++ Lesson5/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace lesson5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int tmp = rnd.Next(1, 100);
+            Console.WriteLine(tmp);
+            int[] arr1 = new int[] { -5, -2, 3, 10, 12 };
+            int[] ress = Solution3(arr1, 10);
+            if (ress.Length == 0)
+                Console.WriteLine("Таких нет");
+            foreach (var elem in ress)
+                Console.WriteLine(elem + " ");
+            Console.WriteLine();
+
+            int[] arr3 = new int[] { 1, 0, 0, 3, 1, 3, 2, 4, 3 };
+            Console.WriteLine(Solution4(arr3, 7));
+        }
+
+        static int Solution4(int[] arr, int target)
+        {
+            int min = 0;
+            int sum = 0;
+            int left = 0, right = 0;
+            for(; right < arr.Length;)
+            {
+                if(sum < target)
+                {
+                    sum += arr[right];
+                    right++;
+                }
+                while(left < arr.Length && sum >= target)
+                {
+                    sum -= arr[left++];
+                }
+                if (min == 0 || min > right - left + 1) min = right-left+1;
+            }
+
+            return min;
+        }
+
+        static int[] Solution3(int[] arr, int target)
+        {
+            int[] res = new int[2];
+            int left = 0, right = arr.Length - 1;
+            for (; left < right;)
+            {
+                if (arr[left] + arr[right] == target)
+                {
+                    res[0] = arr[left];
+                    res[1] = arr[right];
+                    break;
+                }
+                if (arr[left] + arr[right] > target)
+                    right++;
+                else left++;
+            }
+            if (left >= right)
+                res = new int[0];
+
+            return res;
+        }
+    }
+}
+
+
---
+++ lesson_6/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace lesson6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> phone = new Dictionary<string, int>();
+            phone.Add("samsung", 1);
+            phone.Add("iPhone", 2);
+            phone.Add("OPPO", 3);
+
+            string keyCheck = "samsung";
+            if (phone.ContainsKey(keyCheck))
+            {
+                Console.WriteLine($"key: {keyCheck} value:{phone[keyCheck]}");
+            }
+            else
+                Console.WriteLine($"key: {keyCheck} not found");
+
+
+            //удаление элемента
+            string keyRemove = "OPPO";
+            if (phone.Remove(keyRemove))
+                Console.WriteLine($"key{keyRemove}");
+
+            //проверка количества элементов после удаления
+            Console.WriteLine($"Count: {phone.Count}");
+
+        }
+
+        
+        
+    }
+}
+
+
+
+
+       
